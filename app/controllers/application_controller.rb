e68de915934239da4f8e7cfc2c3e40f0919ca7c2
Jacob <jacobamelse@hotmail.com>
2023-05-07 21:29:36 -0500

    finished and tested all of routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error_message
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error_message 
+
+  private 
+
+  def record_not_found_error_message(invalid)
+    render json: { error: "#{invalid.model} not found"}, status: :not_found
+  end
+
+  def unprocessable_entity_error_message(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,37 @@
+class StudentsController < ApplicationController
+
+  before_action :find_student, only: [:show, :update, :destroy]
+
+  def index 
+    render json: Student.all
+  end
+
+  def show 
+    render json: @student
+  end
+
+  def create
+    student = Student.create!(student_params)
+    render json: student, status: :created
+  end
+
+  def update 
+    @student.update!(student_params)
+    reneder json: @student, status: :accepted
+  end
+
+  def destroy
+    @student.destroy
+    render json: {}, status: 202
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.permit(:name, :major, :age)
+  end
+end
